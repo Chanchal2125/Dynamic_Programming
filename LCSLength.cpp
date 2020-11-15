9ff845a916026744f7c0ae84258bdc1d786fb917
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2020-11-15 21:36:31 +0530

    Add files via upload

---
+++ LCSLength.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#define ll             long long int
+#define ld             long double
+#define mod            1000000007
+#define endl           "\n"
+#define vi             vector<ll>
+#define vs             vector<string>
+#define pii            pair<ll, ll>
+#define ump            unordered_map
+#define mp             map
+#define pq_max         priority_queue<ll>
+#define pq_min         priority_queue<ll,vi,greater<ll>
+#define ff             first
+#define ss             second
+#define mid(l,r)       (l+(r-l)/2)
+#define loop(i,a,b)    for(int i=(a); i <=(b);i++)
+#define looprev(i,a,b) for(int i=(a); i >=(b);i--)
+#define clr(val)       memset(val,0,sizeof(val))
+#define what_is(x)     cerr << #x << " is " << x << endl; 
+#define OJ             \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{
+    string s1,s2;
+    ll m,n;
+    cin>>s1>>s2;
+    m = s1.size();
+    n = s2.size();
+    
+    vector <vector <ll>> dp(m+1, vector <ll>(n+1));
+ 
+    loop(i,1,m){
+        loop(j,1,n){
+            if (s1[i-1] == s2[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+            else{
+                dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+    }
+
+    cout<<dp[m][n];
+return 0;
+   
+}
